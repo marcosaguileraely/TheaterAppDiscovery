ab816e962b2f54631322744ddf1614ed9a2f7d13
Marcos Aguilera Ely <marcosaguileraely@gmail.com>
2021-11-16 18:29:03 -0500

    Add: Router rules

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import App from "./App";
+import Detail from "./Detail";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/detail/:id" element={<Detail />} />
+        </Routes>
+    </BrowserRouter>
+    ,
+    document.getElementById("root")
+);
